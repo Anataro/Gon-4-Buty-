573ecf326759f683725342373fd4ac8603735fa0
Anataro <zambazon4@gmail.com>
2017-04-01 14:13:45 +0200

    Pierwszy Tajny Projekt

---
+++ Assets/Skrypty/PozycjaG1.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PozycjaG1 : MonoBehaviour
+{
+    private float stara, dystans, RóżnicaIloraz, różnica, DystansStart,pochodna;
+    private int i;
+    void Start()
+    {
+        i = 1;
+    }
+
+    void Update()
+    {
+		if(i==1) DystansStart = Vector3.Distance(GameObject.FindGameObjectWithTag("Gracz1").transform.position, GameObject.FindGameObjectWithTag("Finish").transform.position);
+        i = 2;
+		dystans = Vector3.Distance(GameObject.FindGameObjectWithTag("Gracz1").transform.position, GameObject.FindGameObjectWithTag("Finish").transform.position);
+        różnica = 1000 * (1 - dystans / DystansStart);
+        pochodna = różnica - stara;
+        stara = różnica;
+        transform.Translate(pochodna, 0, 0);
+    }
+    
+}
