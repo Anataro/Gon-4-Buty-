f4c34b29c5b5058e239de57e6212db237d87ed70
Monakus <monakus00@gmail.com>
2017-04-09 11:35:12 +0200

    Działa

---
+++ Assets/Trzeci tryb/Skrypty/GameManager3.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameManager3 : MonoBehaviour {
+	public GUIText wyścigStart;
+	public GUIText koniec;
+
+	public GameObject gracz1p;
+	public GameObject gracz2p;
+
+	private GameObject gracz1;
+	private GameObject gracz2;
+
+	void Awake()
+	{
+		gracz1 = GameObject.Find (gracz1p.name);
+		gracz2 = GameObject.Find (gracz2p.name);
+	}
+
+	void Start(){
+		StartCoroutine (Odliczanie ());
+	}
+
+	void Update () {
+		if (Input.GetKey (KeyCode.R)) {
+			SceneManager.LoadScene ("Trzeci Tryb");
+		}
+		if (Input.GetKey (KeyCode.Escape)) {
+			Application.Quit ();
+		}
+	}
+
+
+	IEnumerator Odliczanie(){
+		yield return new WaitForSeconds(1);
+		wyścigStart.text = "3";
+		yield return new WaitForSeconds(1);
+		wyścigStart.text = "2";
+		yield return new WaitForSeconds(1);
+		wyścigStart.text = "1";
+		yield return new WaitForSeconds(1);
+		wyścigStart.text = "START!";
+		yield return new WaitForSeconds(0.5f);
+		wyścigStart.text = "";
+		gracz1.GetComponent<SterowanieGracz1> ().start = true;
+		gracz2.GetComponent<SterowanieGracz2> ().start = true;
+
+	}
+
+}
